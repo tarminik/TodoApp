9ab7427fa66966dc74f1c3d1b73fe242c6f55692
tarasov mihail <tarminik@gmail.com>
2024-02-15 15:01:12 +0300

    a

---
+++ backend/todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import TodoList, TodoDetail
+from rest_framework import routers
+
+
+urlpatterns = [
+    path('todo/', TodoList.as_view(), name='todo-list'),
+    path('todo/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
+]
